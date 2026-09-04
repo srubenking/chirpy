c00ae39b77081d6dc536fec51eebaf6fe15c47ef
srubenking <sophie.rubenking@gmail.com>
2026-09-04 17:28:49 +1000

    basic server made, ch1 complete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srubenking/chirpy
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK")) // handle errors here later?
+	})
+
+	server.ListenAndServe()
+}
